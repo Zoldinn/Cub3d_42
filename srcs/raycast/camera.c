ce562851ce9e562d753d7c4418b081755dd5874d
lefoffan - leon <lefoffan@student.42perpignan.fr>
2025-06-10 17:26:43 +0200

    soon dda

--- srcs/raycast/camera.c
+++ srcs/raycast/camera.c
@@ -1,76 +1,94 @@
 #include "../../cub3d.h"
 
-char	get_player_init_dir(t_game *game)
+char	get_player_init_dir(t_map *map)
 {
 	int	i;
 	int	j;
 
 	i = -1;
-	while (++i < game->map.rows)
+	while (++i < map->rows)
 	{
 		j = -1;
-		while (++j < ft_strlen(game->map.map[i]))
+		while (++j < ft_strlen(map->map[i]))
 		{
-			if (is_charset(game->map.map[i][j], "NSWE"))
-				return (game->map.map[i][j]);
+			if (is_charset(map->map[i][j], "NSWE"))
+				return (map->map[i][j]);
 		}
 	}
 	return (0);
 }
 
 // North and south are reversed (because axe Y is reversed)
-void	init_camera_angle(t_game *game)
+void	init_camera_angle(t_camera *camera, t_map *map)
 {
-	char	player;
+	char		player;
 
-	player = get_initial_dir(game);
+	player = get_initial_dir(map);
 	if (player == 'N')
-		game->player.camera.angle_rad = (3 * PI) / 2;
+		camera->angle_rad = (3 * PI) / 2;
 	else if (player == 'S')
-		game->player.camera.angle_rad = PI / 2;
+		camera->angle_rad = PI / 2;
 	else if (player == 'W')
-		game->player.camera.angle_rad = PI;
+		camera->angle_rad = PI;
 	else if (player == 'E')
-		game->player.camera.angle_rad = 0;
+		camera->angle_rad = 0;
 }
 
-void	update_camera_dir(t_game *game)
+void	update_camera_dir(t_camera *camera)
 {
-	double	*dir_x;
-	double	*dir_y;
-	double	*plane_x;
-	double	*plane_y;
-	double	*angle_rad;
-
-	dir_x = &game->player.camera.dir_x;
-	dir_y = &game->player.camera.dir_y;
-	plane_x = &game->player.camera.plane_x;
-	plane_y = &game->player.camera.plane_y;
-	angle_rad = &game->player.camera.angle_rad;
-	*dir_x = cos(*angle_rad);
-	*dir_y = sin(*angle_rad);
-	*plane_x = -(*dir_y) * tan(FOV_DEGREE / 2);
-	*plane_y = *dir_x * tan(FOV_DEGREE / 2);
+	camera->dir[X] = cos(camera->angle_rad);
+	camera->dir[Y] = sin(camera->angle_rad);
+	camera->plane[X] = -camera->dir[Y] * tan(FOV_DEGREE / 2);
+	camera->angle_rad = camera->dir[X] * tan(FOV_DEGREE / 2);
 }
 
-int	*get_raydir_x(t_game *game, int x)
+int	*get_raydir_x(t_camera *camera, t_map *map, int x)
 {
 	int		*raydirs;
 	double	camera_x;
-	double	*dir[2];
-	double	*plane[2];
 
 	raydirs = malloc(sizeof(int) * 2);
 	if (!raydirs)
 		return (NULL);
-	dir[X] = &game->player.camera.dir_x;
-	dir[Y] = &game->player.camera.dir_y;
-	plane[X] = &game->player.camera.plane_x;
-	plane[Y] = &game->player.camera.plane_y;
-	camera_x = 2 * x / (double) game->map.col_max - 1;
-	raydirs[X] = *dir[X] + *plane[X] * camera_x;
-	raydirs[Y] = *dir[Y] + *plane[Y] * camera_x;
+	camera_x = 2 * x / (double) map->col_max - 1;
+	raydirs[X] = camera->dir[X] + camera->plane[X] * camera_x;
+	raydirs[Y] = camera->dir[Y] + camera->plane[Y] * camera_x;
 	return (raydirs);
 }
 
-void	set_step_and_dir
+// length from x/y to x/y side
+void	set_delta_dist(t_camera *camera)
+{
+	if (camera->ray_dir[X] == 0)
+		camera->delta_dist[X] = 1e30;
+	else
+		camera->delta_dist[X] = fabs(1.0 / camera->ray_dir[X]);
+	if (camera->ray_dir[Y] == 0)
+		camera->delta_dist[Y] = 1e30;
+	else
+		camera->delta_dist[Y] = fabs(1.0 / camera->ray_dir[Y]);
+}
+
+void	set_step_and_sidedist(t_player *player, t_camera *camera)
+{
+	double	dist_to_grid;
+
+	camera->step[X] = 1;
+	dist_to_grid = camera->grid_pos[X] + 1 - player->pos_x;
+	camera->side_dist[X] = (dist_to_grid) * camera->delta_dist[X];
+	if (camera->ray_dir[X] < 0)
+	{
+		camera->step[X] = -1;
+		dist_to_grid = player->pos_x - camera->grid_pos[X];
+		camera->side_dist[X] = (dist_to_grid) * camera->delta_dist[X];
+	}
+	camera->step[Y] = 1;
+	dist_to_grid = camera->grid_pos[Y] + 1 - player->pos_y;
+	camera->side_dist[Y] = (dist_to_grid) * camera->delta_dist[Y];
+	if (camera->ray_dir[Y] < 0)
+	{
+		camera->step[Y] = -1;
+		dist_to_grid = player->pos_y - camera->grid_pos[Y];
+		camera->side_dist[Y] = (dist_to_grid) * camera->delta_dist[Y];
+	}
+}
