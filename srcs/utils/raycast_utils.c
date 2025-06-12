eb11159c30f421e6813ec6984bbdd0f5d0c68479
lefoffan - leon <lefoffan@student.42perpignan.fr>
2025-06-12 16:32:20 +0200

    x is for horizontal, y vertical so in a tab of tab, it's tab[y][x]...

--- srcs/utils/raycast_utils.c
+++ srcs/utils/raycast_utils.c
@@ -18,34 +18,6 @@ char	get_player_init_dir(t_map *map)
 	return (0);
 }
 
-// return an int **grid from the char **map
-int	**get_grid_map(t_game *game)
-{
-	int	**grid;
-	int	i;
-	int	j;
-
-	grid = malloc(sizeof(int *) * game->map.rows);
-	if (!grid)
-		return (NULL);
-	i = -1;
-	while (++i < game->map.rows)
-	{
-		grid[i] = malloc(sizeof(int) * ft_strlen(game->map.map[i]));
-		if (!grid[i])
-			return (NULL);
-		j = -1;
-		while (++j < ft_strlen(game->map.map[i]))
-		{
-			if (is_charset(game->map.map[i][j], "NSWE"))
-				grid[i][j] = 0;
-			else
-				grid[i][j] = ft_atoi(game->map.map[i][j]);
-		}
-	}
-	return (grid);
-}
-
 double	get_rad(double degree)
 {
 	return ((degree * PI) / 180);
