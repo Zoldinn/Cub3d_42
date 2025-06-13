684297ec652bb7e0cd822510b0296dbc07f4d00c
Philippe Menard <phil.menard1668@gmail.com>
2025-06-13 17:10:43 +0200

    working on displaying floor and ceiling, still a segfault

--- cub3d.h
+++ cub3d.h
@@ -76,6 +76,14 @@ typedef struct s_camera
 	double	raylength;
 }	t_camera;
 
+typedef struct s_image
+{
+	void	*mlx_img;
+	char	*addr;
+	int		bpp;
+	int		line_len;
+	int		endian;
+}	t_image;
 
 typedef struct s_texture
 {
@@ -104,6 +112,7 @@ typedef struct s_game
 	int			**grid;
 	t_texture	txt[4];
 	t_player	player;
+	t_image		img;
 }	t_game;
 
 typedef enum e_id
--- srcs/rendering/render_map.c
+++ srcs/rendering/render_map.c
@@ -1,9 +1,57 @@
 #include "../../cub3d.h"
 
+void	pix_put(t_image *img, int x, int y, int color)
+{
+	char	*pixel;
+	
+	pixel = img->addr + (y * img->line_len + x * (img->bpp / 8));
+	*(int *)pixel = color;
+}
+
+void	render_background(t_image *img)
+{
+	int	i;
+	int	j;
+	
+	i = 0;
+	while (i < WIDTH / 2)
+	{
+		j = 0;
+		while (j < (HEIGHT / 2))
+		{
+			pix_put(img, j, i, WALL_COLOR);
+			j++;
+		}
+		i++;
+	}
+	i = 0;
+	while (i < WIDTH)
+	{
+		j = HEIGHT / 2;
+		while (j < HEIGHT)
+		{
+			pix_put(img, j, i, FLOOR_COLOR);
+			j++;
+		}
+		i++;
+	}
+}
+
+void	init_img(t_image *img)
+{
+	img->mlx_img = NULL;
+	img->addr = NULL;
+	img->bpp = 0;
+	img->line_len = 0;
+	img->endian = 0;
+}
+
 void	render_map(t_game *game)
 {
-	mlx_put_image_to_window(game->mlx, game->window,
-			game->txt[WALL].img, 0, HEIGHT / 2);
-	mlx_put_image_to_window(game->mlx, game->window,
-			game->txt[FLOOR].img, HEIGHT / 2, HEIGHT);
+	init_img(&game->img);
+	game->img.mlx_img = mlx_new_image(game->mlx, WIDTH, HEIGHT);
+	game->img.addr = mlx_get_data_addr(game->img.mlx_img, &game->img.bpp,
+		&game->img.line_len , &game->img.endian);
+	render_background(&game->img);
+	mlx_put_image_to_window(game->mlx, game->window, game->img.mlx_img, 0, 0);
 }
